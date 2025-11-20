c77dec81678293de79a5d51543be8d38ac5d099e
nikoshaa <hfdzywlld@gmail.com>
2025-11-20 18:25:26 +0700

    Update title & favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,27 @@ export default defineNuxtConfig({
     preset: 'static',
   },
 
-  css: ['~/assets/css/tailwind.css'],
+  app: {
+    head: {
+      title: "Qlinova – Clinic Management System",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Qlinova membantu klinik mengelola operasional, rekam medis elektronik, antrian, farmasi, dan laporan keuangan dalam satu sistem terintegrasi.",
+        },
+      ],
+      link: [
+        // Favicon utama
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
+        // (Opsional) favicon PNG jika kamu pakai
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+      ],
+    },
+  },
+
+  css: ["~/assets/css/tailwind.css"],
 
   postcss: {
     plugins: {
@@ -11,4 +31,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+});
